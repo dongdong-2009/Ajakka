f25ffd7541da146fd6c11314a671e4255c80c05d
pilvikala <michal.brutvan@gmail.com>
2018-02-17 23:13:03 +0200

    remove unnecessary logging

--- Ajakka.Web/routes/index.js
+++ Ajakka.Web/routes/index.js
@@ -4,7 +4,6 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var page = req.query.page;
-  console.log(req.query);
   res.render('index', { title: 'Ajakka', page:page });
 });
 
