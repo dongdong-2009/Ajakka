6b519e86d3b1c172e73c4c5b77fad315fe62a02a
pilvikala <michal.brutvan@gmail.com>
2018-03-12 13:20:47 +0200

    show blacklist service status in about

--- Ajakka.Web/public/js/about/about.js
+++ Ajakka.Web/public/js/about/about.js
@@ -40,7 +40,29 @@ function showDbFailure(error){
     console.log(error);
 }
 
+function testBlacklistConnection(){
+    $.get({
+        url:'./api/blacklist/',
+        success:showBlacklistSuccess,
+        error:showBlacklistFailure
+    })
+}
+
+function showBlacklistSuccess(){
+    let statusElement = $('#blacklistStatus');
+    statusElement.html('OK');
+    statusElement.addClass('text-success')
+}
+
+function showBlacklistFailure(error){
+    let statusElement = $('#blacklistStatus');
+    statusElement.html('ERROR');
+    statusElement.addClass('text-danger')
+    console.log(error);
+}
+
 setTimeout(function(){
     testDbConnection();
     testCollectorConnection();
+    testBlacklistConnection();
 }, 500);
