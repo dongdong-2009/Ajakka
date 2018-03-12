d11f344680cef380baa931da7bb658ff4ca05a22
pilvikala <michal.brutvan@gmail.com>
2018-03-12 13:28:58 +0200

    refactoring

--- Ajakka.Web/public/js/about/about.js
+++ Ajakka.Web/public/js/about/about.js
@@ -1,68 +1,23 @@
-function testCollectorConnection(){
+function testConnection(url, statusDivId ){
     $.get({
-        url:'./api/endpoints/pageCount?pageSize=10',
-        success:showCollectorSuccess,
-        error:showCollectorFailure
+        url:url,
+        success:function(){
+            let statusElement = $(statusDivId);
+            statusElement.html('OK');
+            statusElement.addClass('text-success')
+        },
+        error:function(error){
+            let statusElement = $(statusDivId);
+            statusElement.html('ERROR');
+            statusElement.addClass('text-danger')
+            console.log(error);
+        }
     })
 }
 
-function showCollectorSuccess(){
-    let statusElement = $('#collectorStatus');
-    statusElement.html('OK');
-    statusElement.addClass('text-success')
-}
-
-function showCollectorFailure(error){
-    let statusElement = $('#collectorStatus');
-    statusElement.html('ERROR');
-    statusElement.addClass('text-danger')
-    console.log(error);
-}
-
-function testDbConnection(){
-    $.get({
-        url:'./api/users',
-        success:showDbSuccess,
-        error:showDbFailure
-    })
-}
-
-function showDbSuccess(){
-    let statusElement = $('#dbStatus');
-    statusElement.html('OK');
-    statusElement.addClass('text-success')
-}
-
-function showDbFailure(error){
-    let statusElement = $('#dbStatus');
-    statusElement.html('ERROR');
-    statusElement.addClass('text-danger')
-    console.log(error);
-}
-
-function testBlacklistConnection(){
-    $.get({
-        url:'./api/blacklist/',
-        success:showBlacklistSuccess,
-        error:showBlacklistFailure
-    })
-}
-
-function showBlacklistSuccess(){
-    let statusElement = $('#blacklistStatus');
-    statusElement.html('OK');
-    statusElement.addClass('text-success')
-}
-
-function showBlacklistFailure(error){
-    let statusElement = $('#blacklistStatus');
-    statusElement.html('ERROR');
-    statusElement.addClass('text-danger')
-    console.log(error);
-}
 
 setTimeout(function(){
-    testDbConnection();
-    testCollectorConnection();
-    testBlacklistConnection();
+    testConnection('./api/endpoints/pageCount?pageSize=10', "#collectorStatus");
+    testConnection('./api/users', "#dbStatus");
+    testConnection('./api/blacklist/', "#blacklistStatus");
 }, 500);
