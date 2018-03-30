27eac1fdf4233e683771f01295e8881e2f1f2a61
pilvikala <michal.brutvan@gmail.com>
2018-03-30 17:39:02 +0300

    improved error handling (session timeout caused server to crash)

--- Ajakka.Web/modules/messaging.js
+++ Ajakka.Web/modules/messaging.js
@@ -10,30 +10,48 @@ function generateUuid() {
 
 function SendMessageToQueue(response, message, rpcQueue){
     amqp.connect(configuration.messageQueueHostAddress, function(err, conn) {
+        if(err){
+            console.log('Failed to connect to the message queue host.');
+            console.log(err);
+            response.status(500).send(err);
+            return;
+        }
         conn.createChannel(function(err, ch) {
+            if(err){
+                console.log('Failed to create the channel.');
+                console.log(err);
+                response.status(500).send(err);
+                return;
+            }
             ch.assertQueue('', {exclusive: true}, function(err, q) {
-            var corr = generateUuid();
-            ch.consume(q.queue, function(msg) {
-                if (msg.properties.correlationId == corr) {
-                    var responseObject = JSON.parse(msg.content.toString());
-                    if(responseObject.Error){
-                        console.log("Received error: " + responseObject.Message);
-                        response.status(500).send(responseObject);
-                    }
-                    else{
-                        response.status(200).send(responseObject);
-                    }
-                    
-                setTimeout(function() { conn.close(); }, 500);
+                if(err){
+                    console.log('Failed to connect to the message queue host.');
+                    console.log(err);
+                    response.status(500).send(err);
+                    return;
                 }
-            }, {noAck: true});
-           
-            ch.sendToQueue(rpcQueue,
-            new Buffer(message),
-            { correlationId: corr, replyTo: q.queue });
+                var corr = generateUuid();
+                ch.consume(q.queue, function(msg) {
+                    if (msg.properties.correlationId == corr) {
+                        var responseObject = JSON.parse(msg.content.toString());
+                        if(responseObject.Error){
+                            console.log("Received error: " + responseObject.Message);
+                            response.status(500).send(responseObject);
+                        }
+                        else{
+                            response.status(200).send(responseObject);
+                        }
+                        
+                    setTimeout(function() { conn.close(); }, 500);
+                    }
+                }, {noAck: true});
+            
+                ch.sendToQueue(rpcQueue,
+                new Buffer(message),
+                { correlationId: corr, replyTo: q.queue });
             });
         });
-        });
+    });
 }
 
 module.exports.SendMessageToQueue = SendMessageToQueue;
