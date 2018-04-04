f7db889d8f37e1bc4ebaf2ae2df7a019a167bbf4
pilvikala <michal.brutvan@gmail.com>
2018-04-04 21:39:56 +0300

    macro parser and tests

---
+++ Ajakka.Alerting.Tests/MacroParserTests.cs
@@ -0,0 +1,102 @@
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Ajakka.Alerting.Tests{
+    public class MacroParserTests{
+        dynamic device = new {
+            Mac = "001122334455",
+            Ip = "192.168.1.1",
+            Name = "server"
+        };
+
+        [Fact]
+        void ShouldTranslateMac(){
+            var inputDevice = JsonConvert.SerializeObject(device);
+            var parser = new MacroParser();
+            
+            var parsed = parser.Parse("http://example.com?mac={Mac}",inputDevice);
+            Assert.Equal("http://example.com?mac=001122334455",parsed);
+        }
+
+        [Fact]
+        void ShouldTranslateMacAndIp(){
+            var inputDevice = JsonConvert.SerializeObject(device);
+            var parser = new MacroParser();
+            
+            var parsed = parser.Parse("http://example.com?mac={Mac}&ip={Ip}",inputDevice);
+            Assert.Equal("http://example.com?mac=001122334455&ip=192.168.1.1",parsed);
+        }
+
+        [Fact]
+        void ShouldTranslateMacAndIpAndName(){
+            var inputDevice = JsonConvert.SerializeObject(device);
+            var parser = new MacroParser();
+            
+            var parsed = parser.Parse("http://example.com?mac={Mac}&ip={Ip}&name={Name}",inputDevice);
+            Assert.Equal("http://example.com?mac=001122334455&ip=192.168.1.1&name=server",parsed);
+        }
+
+
+        [Fact]
+        void ShouldNotTranslateNonMacroStrings(){
+            var inputDevice = JsonConvert.SerializeObject(device);
+            var parser = new MacroParser();
+            
+            var parsed = parser.Parse("http://example.com?Mac={Mac}&Ip={Ip}",inputDevice);
+            Assert.Equal("http://example.com?Mac=001122334455&Ip=192.168.1.1",parsed);
+        }
+
+        [Fact]
+        void ShouldIgnoreUnfinishedMacros(){
+            var inputDevice = JsonConvert.SerializeObject(device);
+            var parser = new MacroParser();
+            
+            var parsed = parser.Parse("http://example.com?Mac={Mac}&Ip={Ip",inputDevice);
+            Assert.Equal("http://example.com?Mac=001122334455&Ip={Ip",parsed);
+        }
+
+        [Fact]
+        void ShouldIgnoreUnfinishedMacros2(){
+            var inputDevice = JsonConvert.SerializeObject(device);
+            var parser = new MacroParser();
+            
+            var parsed = parser.Parse("http://example.com?Mac={Mac&Ip={Ip}",inputDevice);
+            Assert.Equal("http://example.com?Mac={Mac&Ip=192.168.1.1",parsed);
+        }
+
+        [Fact]
+        void ShouldIgnoreUnfinishedMacros3(){
+            var inputDevice = JsonConvert.SerializeObject(device);
+            var parser = new MacroParser();
+            
+            var parsed = parser.Parse("http://example.com?Mac={Mac&Ip=1234",inputDevice);
+            Assert.Equal("http://example.com?Mac={Mac&Ip=1234",parsed);
+        }
+        [Fact]
+        void ShouldIgnoreUnfinishedMacros4(){
+            var inputDevice = JsonConvert.SerializeObject(device);
+            var parser = new MacroParser();
+            
+            var parsed = parser.Parse("http://example.com?Mac=}Mac&Ip=1234",inputDevice);
+            Assert.Equal("http://example.com?Mac=}Mac&Ip=1234",parsed);
+        }
+        [Fact]
+        void ShouldIgnoreEmptyMacros(){
+            var inputDevice = JsonConvert.SerializeObject(device);
+            var parser = new MacroParser();
+            
+            var parsed = parser.Parse("http://example.com?Mac={}1",inputDevice);
+            Assert.Equal("http://example.com?Mac={}1",parsed);
+        }
+
+        
+        [Fact]
+        void ShouldNotParseMissingProperties(){
+            var inputDevice = JsonConvert.SerializeObject(device);
+            var parser = new MacroParser();
+            
+            var parsed = parser.Parse("http://example.com?Mac={aaa}1",inputDevice);
+            Assert.Equal("http://example.com?Mac={aaa}1",parsed);
+        }
+    }
+}
---
+++ Ajakka.Alerting/MacroParser.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Ajakka.Alerting{
+    public class MacroParser{
+        public string Parse(string input, string jsonObject) {
+            var deserialized = (JObject) JsonConvert.DeserializeObject(jsonObject);
+            var macros = GetMacros(input);
+            foreach(var item in deserialized){
+                var macro = macros.FirstOrDefault((m)=>m == item.Key);
+                if(macro != null){
+                    input = input.Replace("{"+macro+"}", item.Value.ToString());
+                }
+            }
+            return input;
+        }
+
+        private string[] GetMacros(string input){
+            List<string> macros = new List<string>();
+
+            if(string.IsNullOrEmpty(input))
+                return new string[0];
+
+            int startIndex = 0;
+            int endIndex = 0;
+            while(startIndex <= input.Length){
+                startIndex = input.IndexOf('{', startIndex);
+                if(startIndex == -1){
+                    return macros.ToArray();
+                }
+
+                endIndex = input.IndexOf('}',startIndex);
+                if(endIndex == -1){
+                    return macros.ToArray();
+                }
+                string macro = input.Substring(startIndex + 1, endIndex - startIndex - 1);
+                macros.Add(macro);
+                startIndex++;
+            }
+            return macros.ToArray();
+        }
+    }
+}
