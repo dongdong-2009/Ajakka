2be2b04c5ccac5d0cfd33f0b499161ec1c57a392
pilvikala <michal.brutvan@gmail.com>
2018-04-05 22:00:52 +0300

    sample web server to test out httprequest alerting actions

---
+++ Ajakka.HttpRequestTestServer/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+    console.log(req.query);
+    res.send(req.query);
+ });
+
+app.listen(3030, () => console.log('listening on port 3030'))
